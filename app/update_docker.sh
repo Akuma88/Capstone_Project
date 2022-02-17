f6b10b908c484b8adfc6c206f017e9749dd6280c
Akuma88 <60229601+Akuma88@users.noreply.github.com>
2022-02-17 23:34:40 +0530

    Create update_docker.sh

---
+++ app/update_docker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath=oldappimage
+
+# Step 2:  
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login --username akuma88
+docker tag oldappimage akuma88/oldappimage
+# Step 3:
+# Push image to a docker repository
+docker push akuma88/oldappimage
